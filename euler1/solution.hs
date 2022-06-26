a00e5860bd5dbb13301fdde6e6684976f5ce8314
evf <eliseuv816@gmail.com>
2022-06-26 19:22:38 -0300

    solution to euler1

---
+++ euler1/solution.hs
@@ -0,0 +1,24 @@
+import Control.Arrow ((***))
+import Control.Monad (join)
+
+-- Get a list with all the multiples of the integers in `as` below `n`
+multiples :: Integer -> [Integer] -> [Integer]
+multiples n as = [x | x <- [1 .. n - 1], any (\a -> mod x a == 0) as]
+
+-- Solution to euler1
+-- Sum of all multiples of `as` up to `n`
+solution :: Integer -> [Integer] -> Integer
+solution n as = sum $ multiples n as
+
+-- Map function that applies on tuples
+-- Taken from https://stackoverflow.com/a/9723976
+mapTuple = join (***)
+
+-- Solution to euler1
+-- Sum of all multiples of `a` or `b` below `n`
+solution' :: Integer -> Integer -> Integer -> Integer
+solution' n a b = uncurry subtract ((`mapTuple` (last, sum . init)) ($ map sum [mult a n, mult b n, mult (a * b) n]))
+  where
+    -- List all multiples of `a` below `n`
+    mult :: Integer -> Integer -> [Integer]
+    mult a n = [a * k | k <- [1 .. div (n - 1) a]]
--- euler1/solution.py
+++ euler1/solution.py
@@ -4,7 +4,7 @@ N = 1000
 
 S = 0
 for k in range(1, N):
-    if k % 3 == 0 or k % 5 == k:
+    if k % 3 == 0 or k % 5 == 0:
         S += k
 
 print(S)
