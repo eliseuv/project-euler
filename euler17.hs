9ac7581ce70c200467502117d8289e7831b0b94b
evf <eliseuv816@gmail.com>
2022-09-24 15:45:49 -0300

    update

--- euler17.hs
+++ euler17.hs
@@ -1,19 +1,22 @@
-import Text.Read (Lexeme (String))
+import Data.Char (isAlpha)
 
 -- Say a given integers name
 -- valid from [0..1000]
 sayNumber :: Int -> String
 sayNumber = sayTuple . decimalTuple
   where
+    -- Convert integer into tuple (hudreds, tens, ones)
     decimalTuple :: Int -> (Int, Int, Int)
     decimalTuple x = (xCent, xDec, xUnit)
       where
         xUnit = mod x 10
         xDec = mod (div x 10) 10
         xCent = div x 100
+    -- Say name of number represented by the tuple
     sayTuple :: (Int, Int, Int) -> String
     sayTuple (c, d, u) = centWord ++ optAnd ++ decWord ++ unitWord
       where
+        -- Units are easy enough
         sayUnit :: Int -> String
         sayUnit u = case u of
           1 -> "one"
@@ -26,18 +29,14 @@ sayNumber = sayTuple . decimalTuple
           8 -> "eight"
           9 -> "nine"
           _ -> ""
+        -- Since we are going up tu 1000
         centWord = case c of
           10 -> "onethousand"
           0 -> ""
           _ -> sayUnit c ++ "hundred"
-        optAnd = case c of
-          10 -> ""
-          0 -> ""
-          _ -> case d of
-            0 -> case u of
-              0 -> ""
-              _ -> "and"
-            _ -> "and"
+        optAnd
+          | c `elem` [1 .. 9] && any (/= 0) [d, u] = "and"
+          | otherwise = ""
         decWord = case d of
           1 -> case u of
             0 -> "ten"
@@ -60,15 +59,16 @@ sayNumber = sayTuple . decimalTuple
           8 -> "eighty"
           9 -> "ninety"
           _ -> ""
-        unitWord = case (c, d) of
-          (0, 0) -> case u of
+        unitWord
+          | all (== 0) [c, d] = case u of
             0 -> "zero"
             _ -> sayUnit u
-          _ -> case d of
+          | otherwise = case d of
             1 -> ""
             _ -> sayUnit u
 
 solution :: Int
-solution = length . concatMap sayNumber $ [1 .. 1000]
+solution = length . concatMap (filter isAlpha . sayNumber) $ [1 .. 1000]
 
+solution' :: [String]
 solution' = map sayNumber [1 .. 1000]
